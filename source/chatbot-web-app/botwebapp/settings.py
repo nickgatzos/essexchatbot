38988470a263d998266346c03b87f01b1bfe8b3a
Nick <ng17367@essex.ac.uk>
2017-10-11 22:27:57 +0100

    Huge update
    Added datadir (contains 98290 lines of convolog) | Added some view logic

--- source/chatbot-web-app/botwebapp/settings.py
+++ source/chatbot-web-app/botwebapp/settings.py
@@ -14,8 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'chatbot/templates')
+STATICFILES_DIR = os.path.join(BASE_DIR, 'chatbot/static')
+DATADIR = os.path.join(BASE_DIR, 'chatbot/datadir')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
--- source/chatbot-web-app/chatbot/views.py
+++ source/chatbot-web-app/chatbot/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render, HttpResponse
+from botwebapp.settings import DATADIR
+import os
+import aiml
+import random
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'chatbot/index.html')
+
+    kernel = aiml.Kernel()
+
+    if os.path.isfile(DATADIR + '/bot_brain.brn'):
+        kernel.bootstrap(brainFile=DATADIR + '/bot_brain.brn')
+    else:
+        kernel.bootstrap(learnFiles=DATADIR + "/std-startup.xml", commands="load aiml b")
+        kernel.saveBrain("bot_brain.brn")
+
+    response = kernel.respond("How are you?")
+
+    print(response)
+
+    args = {
+        'msg': response
+    }
+
+    return render(request, 'chatbot/index.html', args)
